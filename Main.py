93c2e4990abcc1314ba14d259a8e2271eb5903ef
JetStarBlues <JetStarBlues@users.noreply.github.com>
2015-11-21 10:59:04 -0700

    testing cgi

---
+++ Main.py
@@ -0,0 +1,110 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+# Home page
+@app.route('/')
+@app.route('/index.html')
+def landingPage():
+	return render_template('index.html')
+
+
+# Thanks page
+@app.route('/thanks.html')
+def thanksPage():
+	return render_template('thanks.html')
+
+
+# Autocomplete script
+@app.route('/auto_python.cgi', methods=['GET', 'POST'])
+def findUsers():
+	if request.method == 'GET':
+		print("why heloo there")
+
+		# Get the query string
+		q = request.query_string   # http://stackoverflow.com/q/11774265
+
+		# Our limited 'database' contains a few users
+		# with their username and full name
+		data = [
+			{
+				"user" : "amon",
+				"name" : "N. Equalist"
+			},
+			{
+				"user" : "android18",
+				"name" : "Bad Ass"
+			},
+			{
+				"user" : "ayeka",
+				"name" : "Crown Princess of the Imperial Family of Jurai"
+			},
+			{
+				"user" : "gaara",
+				"name" : "Sandy"
+			},
+			{
+				"user" : "goku",
+				"name" : "Kakarot"
+			},
+			{
+				"user" : "hiruma",
+				"name" : "Yoichi Hiruma"
+			},
+			{
+				"user" : "naruto",
+				"name" : "The next Hokage"
+			},
+			{
+				"user" : "rockLee",
+				"name" : "Rock Lee"
+			},
+			{
+				"user" : "ryoko",
+				"name" : "Space Pirate Ryoko"
+			},
+			{
+				"user" : "sakura",
+				"name" : "S&S Forever"
+			},
+			{
+				"user" : "sasuke",
+				"name" : "Sasuke Uchiha"
+			},
+			{
+				"user" : "vegeta",
+				"name" : "Saiyan Prince"
+			}
+		]
+
+		# We "search" through the data
+		regex = re.escape( q['to'].value )
+
+		for row in data:
+			# Looking for users that match our auto-complete search
+				# stackoverflow.com/a/14225664/2354735
+				# stackoverflow.com/a/500870/2354735
+			if ( re.search(regex, row["user"], re.IGNORECASE) is not None or 
+			     re.search(regex, row["name"], re.IGNORECASE) is not None ):  
+				
+				return ( '<li id="' + row["user"] + '">' +
+						'<img class="icon" src="' + "{{ url_for('static', filename='') }}" + 'icons/' + row["user"] + '.png"/>' +
+						'<div id="uDetails">' +
+						'<span id="username">' + row["user"] + '</span>' +
+						'<span> : </span>' +
+				    	'<span id="fullname">' + row["name"] + '</span>' +
+				    	'</div></li>' )
+
+
+
+
+
+
+# Start Server
+if __name__ == '__main__':
+	# app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
